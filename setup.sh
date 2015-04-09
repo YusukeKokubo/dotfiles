106d338d3eca4cb8a0c075a95857c2c3489e18fd
YusukeKokubo <yusuke.kokubo@gmail.com>
2015-04-09 16:36:00 +0900

    add .tigrc and setting I to rebase -i

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gemrc ~/.gemrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tigrc ~/.tigrc
