a7434083d022e82505f48b504923836ba5fe7606
YusukeKokubo <yusuke.kokubo@gmail.com>
2017-03-05 00:23:34 +0900

    added tmux.conf

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ ln -sf ~/dotfiles/.gemrc ~/.gemrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
 ln -sf ~/dotfiles/.zshrc.custom ~/.zshrc.custom
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 ln -sf ~/dotfiles/hammerspoon/init.lua ~/.hammerspoon/init.lua
 
