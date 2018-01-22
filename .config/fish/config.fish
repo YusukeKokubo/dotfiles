85ed8d380cc2baac2c1ef42b42b29ff96b7849fe
YusukeKokubo <yusuke.kokubo@gmail.com>
2018-01-22 21:35:34 +0900

    add config for fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+  bind \c] peco_select_ghq_repository
+end
+
+balias g git
+balias v vim
+
+set -x GOPATH ~/gohome
+set -x GCLOUD /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/
+set -x GHQ_ROOT ~/src
+
+set PATH $GCLOUD/bin $PATH
--- setup.sh
+++ setup.sh
@@ -8,5 +8,8 @@ ln -sf ~/dotfiles/.tigrc ~/.tigrc
 ln -sf ~/dotfiles/.zshrc.custom ~/.zshrc.custom
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
+mkdir -p ~/.config/fish
+ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
+
 ln -sf ~/dotfiles/hammerspoon/init.lua ~/.hammerspoon/init.lua
 
