0bc7d980ac39347da054441affe08bd8526cb28a
YusukeKokubo <yusuke.kokubo@gmail.com>
2021-01-13 09:58:51 +0900

    good-bye zsh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gemrc ~/.gemrc
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
