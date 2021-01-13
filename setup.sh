245d100afcc5b73c15ded11a5b145691b626a66a
YusukeKokubo <yusuke.kokubo@gmail.com>
2021-01-13 09:54:56 +0900

    Thank you hammerspoon and good-bye

--- setup.sh
+++ setup.sh
@@ -11,8 +11,6 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/fish
 ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
 
-ln -sf ~/dotfiles/hammerspoon/init.lua ~/.hammerspoon/init.lua
-
 ln -sf ~/dotfiles/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
 ln -sf ~/dotfiles/.config/karabiner/assets/complex_modifications/dynalist.json ~/.config/karabiner/assets/complex_modifications/dynalist.json
 ln -sf ~/dotfiles/.config/karabiner/assets/complex_modifications/slack.json ~/.config/karabiner/assets/complex_modifications/slack.json
