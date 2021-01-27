64b62a3112c6235ebea50cea00be66a880e1b4dc
YusukeKokubo <yusuke.kokubo@gmail.com>
2021-01-27 10:51:13 +0900

    no longer using zsh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,7 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gemrc ~/.gemrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
-ln -sf ~/dotfiles/.zshrc.custom ~/.zshrc.custom
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 mkdir -p ~/.config/fish
