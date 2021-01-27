5e1eb4800ccc5eadf15e5f8bea813d6d56680725
YusukeKokubo <yusuke.kokubo@gmail.com>
2021-01-27 10:52:16 +0900

    setup for my M1 Mac

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,11 @@
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
   bind \c] peco_select_ghq_repository
-end
 
-balias g git
-balias v vim
+  # to install plugin functions
+  # fisher install oh-my-fish/plugin-peco
+  # fisher install yoshiori/fish-peco_select_ghq_repository
+end
 
 set -x GOPATH ~/gohome
 set -x GCLOUD /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
@@ -14,3 +15,6 @@ set -x GHQ_ROOT ~/src
 set PATH $GCLOUD/bin $PATH
 set PATH $GAE $PATH
 
+set PATH /Applications/Visual\ Studio\ Code\ -\ Insiders.app/Contents/Resources/app/bin $PATH
+set PATH /opt/homebrew/bin/brew $PATH
+set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths
